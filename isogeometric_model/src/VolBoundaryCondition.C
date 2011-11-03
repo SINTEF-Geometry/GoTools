0d68babc679eee9eb022e6212992ed669ea993f7
sbr <sbr@sintef.no>
2011-11-03 10:07:06 +0000

    Added empty source file (code defined out to build, suspecting circular inheritance is the culprit, no time to fix now).

---
+++ isogeometric_model/src/VolBoundaryCondition.C
@@ -0,0 +1,102 @@
+//===========================================================================
+//                                                                           
+// File: VolBoundaryCondition.C                                              
+//                                                                           
+// Created: Tue Nov  1 10:17:18 2011                                         
+//                                                                           
+// Author: Sverre Briseid <Sverre.Briseid@sintef.no>
+//                                                                           
+// Revision: $Id$
+//                                                                           
+// Description:
+//                                                                           
+//===========================================================================
+
+
+#if 0
+
+#include "GoTools/isogeometric_model/VolBoundaryCondition.h"
+
+
+namespace Go
+{
+
+  //===========================================================================
+  VolBoundaryCondition::VolBoundaryCondition(int face_nmb, BdConditionType type,
+					     BdCondFunctor *fbd,
+					     std::vector<std::pair<double, double> >& domain)
+    : BlockBoundaryCondition(type),
+      fbd_(fbd),
+      domain_(domain)
+  //===========================================================================
+  {
+    MESSAGE("VolBoundaryCondition() under construction!");
+  }
+
+  //===========================================================================
+  VolBoundaryCondition::~VolBoundaryCondition()
+  //===========================================================================
+  {
+    MESSAGE("~() not implemented");
+  }
+
+  //===========================================================================
+  void 
+  VolBoundaryCondition::getCoefficientsEnumeration(std::vector<int>& local_enumeration)
+  //===========================================================================
+  {
+    MESSAGE("getCoefficientsEnumeration() not implemented");
+  }
+
+  //===========================================================================
+  void
+  VolBoundaryCondition::getBdCoefficients(std::vector<std::pair<int, Point> >& coefs)
+  //===========================================================================
+  {
+    MESSAGE("getBdCoefficients() not implemented");
+  }
+
+  //===========================================================================
+  void VolBoundaryCondition::getBasisFunctions(int index_of_Gauss_point1,
+					       int index_of_Gauss_point2,
+					       std::shared_ptr<BasisDerivs> result,
+					       int solutionspace_idx) const
+  //===========================================================================
+  {
+    MESSAGE("getBasisFunctions() not implemented");
+  }
+
+  //===========================================================================
+  std::shared_ptr<SplineSurface> VolBoundaryCondition::getSplineApproximation() const
+  //===========================================================================
+  {
+    MESSAGE("getSplineApproximation() not implemented");
+    std::shared_ptr<SplineSurface> sf;
+    return sf;
+  }
+
+  //===========================================================================
+  void VolBoundaryCondition::update()
+  //===========================================================================
+  {
+    MESSAGE("update() not implemented");
+  }
+
+  //===========================================================================
+  void VolBoundaryCondition::updateBoundaryValue(BdCondFunctor* fbd)
+  //===========================================================================
+  {
+    MESSAGE("updateBoundaryValue() not implemented");
+  }
+
+  //===========================================================================
+  tpTolerances VolBoundaryCondition::getTolerances() const
+  //===========================================================================
+  {
+    return parent_->getTolerances();
+  }
+
+}
+
+
+#endif
