c441784a68a17dd151933333b9433741a2bfea66
jbt <jbt@sintef.no>
2011-11-11 15:44:34 +0000

    Updated Doxygen documentation

--- intersections/include/GoTools/intersections/intersections-doxymain.h
+++ intersections/include/GoTools/intersections/intersections-doxymain.h
@@ -1,13 +1,6 @@
-/// \mainpage GoTools Intersections
+/// \page intersections GoTools Intersections
 ///
 /// Contains tools for finding intersections of spline curves and surfaces.
 ///
-/// This module depends on:
-/// - The GoTools Core library
-/// - The GoTools Implicitization toolkit
-/// - SISL
-/// - The newmat matrix library, which is freely available from <a
-/// href ="http://www.robertnz.net/">www.robertnz.net</a>.
-
-/// \namespace Go
-/// The Go namespace is the common namespace for all GoTools modules.
+/// This module depends on the newmat matrix library, which is freely
+/// available from <a href="http://www.robertnz.net/">www.robertnz.net</a>.
--- trivariate/include/GoTools/trivariate/trivariate-doxymain.h
+++ trivariate/include/GoTools/trivariate/trivariate-doxymain.h
@@ -3,13 +3,6 @@
 //                        DOCUMENTATION ON MAIN PAGE
 //===========================================================================
 
-/// \mainpage GoTools Trivariate Documentation
+/// \page trivariate GoTools Trivariate
 ///
 /// Contains tools for creating volumes.
-/// 
-/// This module depends on:
-/// - The GoTools Core library
-
-
-/// \namespace Go
-/// This namespace is the common namespace for all GoTools modules.
