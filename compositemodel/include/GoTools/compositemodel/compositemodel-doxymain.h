9cd73dc4371151b0a126ca9f16b54da0da893d49
jbt <jbt@sintef.no>
2012-03-13 14:58:58 +0000

    Updated Doxygen documentation

--- compositemodel/include/GoTools/compositemodel/compositemodel-doxymain.h
+++ compositemodel/include/GoTools/compositemodel/compositemodel-doxymain.h
@@ -9,25 +9,31 @@
 ///
 /// Contains tools for representing composite models.
 ///
+/// \if null
 /// An overview documentation for this module can be found in
 /// \ref compositemodel_doc.
-/// 
-/// This module depends on:
-/// - The GoTools Core library
+/// \endif
+///  
+/// This module depends on the following GoTools modules:
+/// - GoTools Core
 /// - GoTools Implicitization
 /// - GoTools Intersection
 /// - GoTools Parametrization
-/// - GoTools ttl
-/// - GoTools igeslib
-/// - SISL
-/// - GoTools topology
+/// - GoTools Igeslib
+/// - GoTools Topology
+///
+/// and the following modules external to GoTools:
+/// - SISL (SINTEF)
+/// - TTL (SINTEF)
 /// - The newmat matrix library, which is freely available from <a
 /// href ="http://www.robertnz.net/">www.robertnz.net</a>.
+///
+/// SISL, TTL and newmat is included for convenience.
 
-/// \namespace Go
-/// The Go namespace is the common namespace for all GoTools modules.
+// /// \namespace Go
+// /// The Go namespace is the common namespace for all GoTools modules.
 
-/// \namespace hetriang
-/// Namespace for classes in the half-edge data structure.
+// /// \namespace hetriang
+// /// Namespace for classes in the half-edge data structure.
 
 #endif // _COMPOSITEMODEL_DOXYMAIN_H
