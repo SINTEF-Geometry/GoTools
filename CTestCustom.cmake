1f9b34308c31ae398c4617127d86af9ab335e585
jbt <jbt@sintef.no>
2012-09-28 13:22:49 +0000

    Minor mods

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -1,7 +1,6 @@
 IF(WIN32)
     SET(CTEST_CUSTOM_POST_TEST
       "C:\Windows\System32\cmd /C type Testing\Temporary\LastTest.log")
-#    SET(CTEST_CUSTOM_POST_TEST "type Testing\Temporary\LastTest.log")
 ELSE(WIN32)
     SET(CTEST_CUSTOM_POST_TEST "cat Testing/Temporary/LastTest.log")
 ENDIF(WIN32)
