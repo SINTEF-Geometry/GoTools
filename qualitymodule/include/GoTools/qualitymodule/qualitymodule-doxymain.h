5d32778dd10585a33cb3ef4d23bb6e3a68c8c99b
jbt <jbt@sintef.no>
2013-01-22 14:21:02 +0000

    Preparing various examples in QualityModule.

---
+++ qualitymodule/include/GoTools/qualitymodule/qualitymodule-doxymain.h
@@ -0,0 +1,8 @@
+#ifndef _QUALITYMODULE_DOXYMAIN_H
+#define _QUALITYMODULE_DOXYMAIN_H
+
+/// \page qualitymodule GoTools Qualitymodule
+///
+/// Contains tools for checking quality...
+
+#endif // _QUALITYMODULE_DOXYMAIN_H
