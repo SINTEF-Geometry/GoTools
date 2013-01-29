f14fd6b3eaccff248ca53a0fd41fb4e83b0a65f0
sbr <sbr@sintef.no>
2013-01-29 15:05:16 +0000

    Added fullTensorProductSurface() (creates regular SplineSurface version through refinement).

--- lrsplines2D/app/benchmarkLRSurfacePointEval.C
+++ lrsplines2D/app/benchmarkLRSurfacePointEval.C
@@ -25,6 +25,7 @@
 #include "GoTools/geometry/ObjectHeader.h"
 #include "GoTools/geometry/SplineSurface.h"
 #include "GoTools/geometry/GeometryTools.h"
+#include "GoTools/geometry/ClassType.h"
 #include "GoTools/utils/config.h"
 
 
@@ -35,10 +36,12 @@
 #include "string.h"
 
 
-using Go::Header;
+
+using Go::ObjectHeader;
 using Go::SplineSurface;
 using Go::LRSplineSurface;
 
+using namespace Go;
 
 // Assuming the domain is the same.
 double maxDist(const SplineSurface& spline_sf,
@@ -100,166 +103,14 @@ int main(int argc, char *argv[])
       return -1;
     }
 
-  // order_u = spline_sf->order_u();
-  // order_v = spline_sf->order_v();
-  num_coefs_u = spline_sf->numCoefs_u();
-  num_coefs_v = spline_sf->numCoefs_v();
-  // We must count the number of segments. We do this the easiest way
-  // by looking at the number of different knots in each direction,
-  // (and subtract one).
-  vector<double> all_knots_u(spline_sf->basis_u().begin(), spline_sf->basis_u().end());
-  vector<double> all_knots_v(spline_sf->basis_v().begin(), spline_sf->basis_v().end());
-  vector<double>::iterator last_uniq_u = unique(all_knots_u.begin(), all_knots_u.end(), my_equal_function);
-  vector<double>::iterator last_uniq_v = unique(all_knots_v.begin(), all_knots_v.end(), my_equal_function);
-  vector<double> unique_knots_u(all_knots_u.begin(), last_uniq_u);
-  vector<double> unique_knots_v(all_knots_v.begin(), last_uniq_v);
+  double max_dist = maxDist(*spline_sf, *lr_spline_sf, num_dir_samples, num_dir_samples);
 
-  int num_segs_u = unique_knots_u.size() - 1;
-  int num_segs_v = unique_knots_v.size() - 1;
-  std::cout << "num_segs_u: " << num_segs_u << ", num_segs_v: " << num_segs_v << std::endl;
+  std::cout << "Max dist: " << max_dist << std::endl;
 
-  // We create the refinement vectors.
-  // First segments with a constant u parameter.
-  vector<LRSplineSurface::Refinement2D> refs_u, refs_v;
-  int mult = 1;
-  Direction2D dir = XFIXED; // XDIRFIXED, refining in the x-dir (u).
-  for (size_t ki = 0; ki < num_segs_u - 1; ++ki)
-  {
-      // We bisect the interval.
-      double wgt = 0.4; // Value in the range [0.0, 1.0].
-      double ref_par = wgt*unique_knots_u[ki] + (1.0 - wgt)*unique_knots_u[ki+1];
-
-      // We insert line segments which cover order_v - 1 intervals.
-      for (size_t kj = order_v - 1; kj < num_segs_v; kj += order_v)
-      {
-	  // To avoid a too regular pattern we alter start
-	  // values. Maybe we should use a random function ...
-	  double tmin = (ki%2 == 1) ? unique_knots_v[kj - order_v + 1] : unique_knots_v[kj - order_v + 2];
-	  double tmax = (ki%2 == 1) ? unique_knots_v[kj] : unique_knots_v[kj + 1];
-
-	  LRSplineSurface::Refinement2D ref;
-	  ref.kval = ref_par;
-	  ref.start = tmin;
-	  ref.end = tmax;
-	  ref.d = dir;
-	  ref.multiplicity = mult;
-	  refs_u.push_back(ref); // Refinements in the u-dir.
-      }
-  }
-
-  dir = YFIXED; // YDIRFIXED, refining in the y-dir (v).
-  for (size_t ki = 0; ki < num_segs_v - 1; ++ki)
-  {
-      // We bisect the interval.
-      double wgt = 0.4;
-      double ref_par = wgt*unique_knots_v[ki] + (1.0 - wgt)*unique_knots_v[ki+1];
-
-      // We insert line segments which cover order_u - 1 intervals.
-      for (size_t kj = order_u - 1; kj < num_segs_u; kj += order_u)
-      {
-	  // To avoid a too regular pattern we alter start
-	  // values. Maybe we should use a random function ...
-	  double tmin = (ki%2 == 1) ? unique_knots_u[kj - order_u + 1] : unique_knots_u[kj - order_u + 2];
-	  double tmax = (ki%2 == 1) ? unique_knots_u[kj] : unique_knots_u[kj + 1];
-
-	  LRSplineSurface::Refinement2D ref;
-	  ref.kval = ref_par;
-	  ref.start = tmin;
-	  ref.end = tmax;
-	  ref.d = dir;
-	  ref.multiplicity = mult;
-	  refs_v.push_back(ref); // Refinements in the v-dir.
-      }
-  }
-
-  vector<LRSplineSurface::Refinement2D> all_refs(refs_u.begin(), refs_u.end());
-  all_refs.insert(all_refs.end(), refs_v.begin(), refs_v.end());
-  // if (all_refs.size() > 0)
-  //     all_refs.pop_back();
-  // if (all_refs.size() > 0)
-  //     all_refs.pop_back();
-  // if (all_refs.size() > 0)
-  //     all_refs.pop_back();
-  std::cout << "num_refs_to_insert: " << all_refs.size() << std::endl;
-
-//  all_refs.erase(all_refs.begin() + 1, all_refs.begin() + 3);
-//  all_refs.erase(all_refs.begin() + 4, all_refs.end());
-//  all_refs.erase(all_refs.begin(), all_refs.begin() + 2);
-  std::cout << "num_refs_to_insert: " << all_refs.size() << std::endl;
-
-
-  // We then create and refine the LRSplineSurface.
-
-  lr_spline_sf_single_refs = shared_ptr<LRSplineSurface>
-      // (new LRSpline<vector<double>::const_iterator, vector<double>::const_iterator> >
-      (new LRSplineSurface(order_u - 1, order_v - 1,
-			   num_coefs_u, num_coefs_v,
-			   dim,
-			   spline_sf->basis_u().begin(),
-			   spline_sf->basis_v().begin(),
-			   spline_sf->coefs_begin()));
-
-  bool refine_multi = true;
-  if (refine_multi)
-  {
-      double time_lrspline_lr_ref = benchmarkSfRefinement(*lr_spline_sf, all_refs);
-      std::cout << "Time lr refinement: " << time_lrspline_lr_ref << std::endl;
-      // We write to screen the number of basis functions for both
-      // versions.
-      int num_elem = lr_spline_sf->numElements();
-      int num_basis_funcs = lr_spline_sf->numBasisFunctions();
-      std::cout << "num_elem: " << num_elem << ", num_basis_funcs: " << num_basis_funcs << std::endl;
-
-      double max_dist = maxDist(spline_sf.get(), *lr_spline_sf, num_samples_u, num_samples_v);
-      std::cout << "Max dist between input and (multi) refined surface: " << max_dist << std::endl;
-
-      std::ofstream fileout("tmp/ref_lr_multi.g2");
-      lr_spline_sf->writeStandardHeader(fileout);
-      lr_spline_sf->write(fileout);
-
-      // MESSAGE("Missing writing refined surface grid to file!");
-      std::ofstream lrsf_grid_ps("tmp/lrsf_grid.ps");
-//  writePostscriptMesh(*lrsf);
-      writePostscriptMesh(*lr_spline_sf, lrsf_grid_ps);
-
-  }
-
-  bool refine_single = true;
-  if (refine_single)
-  {
-      double time_lrspline_lr_ref_single = benchmarkSfRefinement(*lr_spline_sf_single_refs, all_refs, true);
-      std::cout << "Time lr refinement single refs: " << time_lrspline_lr_ref_single << std::endl;
-      int num_elem_single = lr_spline_sf_single_refs->numElements();
-      int num_basis_funcs_single = lr_spline_sf_single_refs->numBasisFunctions();
-      std::cout << "num_elem_single_refs: " << num_elem_single <<
-	  ", num_basis_funcs_single_refs: " << num_basis_funcs_single << std::endl;
-
-#if 0//ndef NDEBUG
-  vector<LRBSpline2D*> bas_funcs;
-  for (auto iter = lr_spline_sf_single_refs->basisFunctionsBegin(); iter != lr_spline_sf_single_refs->basisFunctionsEnd(); ++iter)
-    {
-      bas_funcs.push_back((*iter).second.get());
-    }
-  vector<Element2D*> elems;
-  for (auto iter = lr_spline_sf_single_refs->elementsBegin(); iter != lr_spline_sf_single_refs->elementsEnd(); ++iter)
-  {
-      elems.push_back(((*iter).second.get()));
-  }
-  puts("Remove when done debugging!");
-#endif
-
-      double max_dist_post_ref_multi_ref = maxDist(spline_sf.get(), *lr_spline_sf_single_refs, num_samples_u, num_samples_v);
-      std::cout << "Max dist input and (single) ref surface: " << max_dist_post_ref_multi_ref << std::endl;
-
-      std::ofstream fileout2("tmp/ref_lr_single.g2");
-      lr_spline_sf_single_refs->writeStandardHeader(fileout2);
-      lr_spline_sf_single_refs->write(fileout2);
-  }
 }
 
 
-
-double maxDist(const SplineSurface* param_sf,
+double maxDist(const SplineSurface& spline_sf,
 	       const LRSplineSurface& lr_spline_sf,
 	       int num_samples_u, int num_samples_v)
 {
@@ -283,7 +134,7 @@ double maxDist(const SplineSurface* param_sf,
 	for (int ki = 0; ki < num_samples_u; ++ki)
 	{
 	    double upar = umin + ki*ustep;
-	    param_sf->point(go_pt, upar, vpar);
+	    spline_sf.point(go_pt, upar, vpar);
 	    lr_spline_sf.point(lr_pt, upar, vpar);
 	    double dist = go_pt.dist(lr_pt);
 	    if (dist > max_dist)
@@ -309,3 +160,4 @@ double maxDist(const SplineSurface* param_sf,
 
 
 }
+
