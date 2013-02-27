c049875091deefb7f439b3842d6a3e214c30faca
jbt <jbt@sintef.no>
2013-02-27 12:00:06 +0000

    Updates related to GPL version 4.2.0.

---
+++ isogeometric_model/include/GoTools/isogeometric_model/isogeometric_model-doxymain.h
@@ -0,0 +1,8 @@
+#ifndef _ISOGEOMETRIC_MODEL_DOXYMAIN_H
+#define _ISOGEOMETRIC_MODEL_DOXYMAIN_H
+
+/// \page isogeometric_model GoTools IsogeometricModel
+///
+/// Contains tools related to isogeometric analysis.
+
+#endif // _ISOGEOMETRIC_MODEL_DOXYMAIN_H
---
+++ lrsplines2D/include/GoTools/lrsplines2D/lrsplines2d-doxymain.h
@@ -0,0 +1,8 @@
+#ifndef _LRSPLINES2D_DOXYMAIN_H
+#define _LRSPLINES2D_DOXYMAIN_H
+
+/// \page lrsplines2d GoTools LRsplines2D
+///
+/// Contains classes and tools for working with LR spline surfaces.
+
+#endif // _LRSPLINES2D_DOXYMAIN_H
