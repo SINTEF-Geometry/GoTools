288692a0092c113e0cdc829a00e36e53524701bf
Kjell Fredrik Pettersen <kfp@sintef.no>
2014-06-10 14:13:34 +0200

    Fine registration added

--- gotools-core/include/GoTools/utils/RegistrationUtils.h
+++ gotools-core/include/GoTools/utils/RegistrationUtils.h
@@ -47,15 +47,106 @@
 
 namespace Go
 {
+  /// Enumerator for registration error reasons
+  /// RegistrationOK   No error found
+  /// TooFewPoints     Less than three points are given
+  /// PointSetSizeDiff The two point sets given have different length
+  /// PointsColinear   Failed to find a tripple of points that are not on the same line
+  /// SolveFailed      Solving the linear system in the Newtons approach method failed.
+  ///                  Error code is stored in registrationData.solve_code
+  enum RegistrationReturnType { RegistrationOK, TooFewPoints, PointSetSizeDiff, PointsColinear, SolveFailed };
 
-  /// Given two sequences of points in 3D, get the rotation, rescaling and translation that sends the second point set
+  /// Struct for input to fine registration
+  struct RegistrationInput
+  {
+    /// Maximum number of iterations in newton approach method
+    int max_newton_iterations_ = 10;
+
+    /// Upper limit for when weighed sum of squares of changes in Newton's method requires us to break out of the loop
+    double newton_tolerance_ = 1.0e-8;
+
+    /// If true, the result of the first iteration of Newtons method will set the weights
+    /// for the calculation of the sqaures of changes used to check if we have reached a satisfactory
+    /// result and should break out of Newtons method. The weights will be set relatively according
+    /// to the changes, with 1.0 as the weight for the translation vector
+    bool calculate_tolerance_weights_ = false;
+
+    /// The weight of the square of the change in the rotation vector, used in the Newtons method
+    double tolerance_weight_rotation_ = 1.0;
+
+    /// The weight of the square of the change in the translation vector, used in the Newtons method
+    double tolerance_weight_translation_ = 1.0;
+
+    /// The weight of the square of the change in the rescaling, used in the Newtons method
+    double tolerance_weight_rescale_ = 1.0;
+
+    /// The maximum number of iterations used when solving the linear system in each iteration in Newtons method
+    int max_solve_iterations_ = 150;
+
+    /// The tolerance used when solving the linear system in each iteration in Newtons method
+    double solve_tolerance_ = 1.0e-8;
+
+    /// Set the tolerance weights used in Newtons method
+    void setToleranceWeights(double w_rotation, double w_translation, double w_rescaling)
+    {
+      tolerance_weight_rotation_ = w_rotation;
+      tolerance_weight_translation_ = w_translation;
+      tolerance_weight_rescale_ = w_rescaling;
+      calculate_tolerance_weights_ = false;
+    }
+  };
+
+  /// Struct for result from registration process, either raw, fine or combined
+  struct RegistrationResult
+  {
+    /// The result type of the registration process, either OK or an error code
+    RegistrationReturnType result_type_;
+
+    /// The rotation matrix of the registration
+    std::vector<std::vector<double> > rotation_matrix_;
+
+    /// The translation of the registration (to be performed after rescaling and rotation)
+    Point translation_;
+
+    /// The rescaling factor of the registration;
+    double rescaling_;
+
+    /// The iteration number (starting at 0) of the last newton iteration,
+    /// or the maximum number of iterations if the weighed sum of squares of changes
+    /// never became smaller than the tolerance limit.
+    /// Only used for fine registration
+    int last_newton_iteration_;
+
+    /// The last weighed sum of squares of changes in Newton's method
+    /// Only used for fine registration
+    double last_change_;
+
+    /// The return value of the last attempt to solve the linear system in the Newtons method iterations
+    /// Only used for fine registration
+    int solve_result_;
+  };
+
+  /// Given two sequences of points in 3D, get an approximate rotation, rescaling (optional) and translation that sends the second
+  /// point set close to the first. The point sets should be close (but not necessarily identical) in shape, with matching
+  /// points coming in the same order (i.e. point number i in the two sequences should be close to each other after performing
+  /// the transformation on the second point set), but the original coordinate represenation of the two sets might be totally
+  /// different.
+  RegistrationResult rawRegistration(const std::vector<Point>& points_fixed, const std::vector<Point>& points_transform, bool allow_rescaling);
+
+  /// Given two sequences of points in 3D, get the rotation, rescaling (optional) and translation that sends the second point set
   /// as close as possible to the first (i.e. that minimizes the sum of the square distances). The sequences must be of
   /// same length (at least three), and the points will be matched in the order they come in the vectors, i.e.
-  /// points_transform[i] should, after the transformation, be as close as possible to points_fixed[i].
-  /// The rotation matrix (together with the rescaling) is stored in 'rotation_matrix', the tronslation (to be performed
-  /// after the roation) is stored in 'translate'
-  bool registration(const std::vector<Point>& points_fixed, const std::vector<Point>& points_transform,
-		    std::vector<std::vector<double> >& rotation_matrix, Point& translate);
+  /// points_transform[i] should, after the transformation, be close to points_fixed[i].
+  /// The method works best if the point sets already are close to each other, but not necessarily in optimal position.
+  RegistrationResult fineRegistration(const std::vector<Point>& points_fixed, const std::vector<Point>& points_transform,
+				      bool allow_rescaling, RegistrationInput params);
+
+  /// Given two sequences of points in 3D, get the rotation, rescaling (optional) and translation that sends the second point set
+  /// as close as possible to the first (i.e. that minimizes the sum of the square distances). The sequences must be of
+  /// same length (at least three), and the points will be matched in the order they come in the vectors, i.e.
+  /// points_transform[i] should, after the transformation, be close to points_fixed[i].
+  RegistrationResult registration(const std::vector<Point>& points_fixed, const std::vector<Point>& points_transform,
+				  bool allow_rescaling, RegistrationInput params);
 
 } // namespace Go
 
