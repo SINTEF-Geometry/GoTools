49001b59a58c600767772078cb7a96f3c51c9f45
Vibeke Skytt <Vibeke.Skytt@sintef.no>
2015-01-27 13:32:33 +0100

    Ready to push to working

--- gotools-core/app/geometry/SplitHalfBounded.C
+++ gotools-core/app/geometry/SplitHalfBounded.C
@@ -37,41 +37,51 @@
  * written agreement between you and SINTEF ICT. 
  */
 
-#ifndef _GVAPPLICATIONVOLANDLR_H
-#define _GVAPPLICATIONVOLANDLR_H
 
 
-#include "GoTools/viewlib/gvApplication.h"
-#include "GoTools/tesselator/GeneralMesh.h"
+#include <fstream>
+#include "GoTools/geometry/ObjectHeader.h"
+#include "GoTools/geometry/BoundedSurface.h"
+#include "GoTools/geometry/GoTools.h"
 
 
-class gvApplicationVolAndLR : public gvApplication
+using namespace std;
+using namespace Go;
+
+
+int main(int argc, char** argv)
 {
+  GoTools::init();
 
-Q_OBJECT
+  if (argc != 3) {
+    cout << "Usage:  " << argv[0] << " infile outfile" << endl;
+    return 1;
+  }
 
+  ifstream ins(argv[1]);
+  ObjectHeader header;
+  BoundedSurface* in_surf = new BoundedSurface();
+  ins >> header;
+  ins >> *in_surf;
+  ins.close();
 
-public:
-    gvApplicationVolAndLR(std::auto_ptr<DataHandler> dh,
-			  QWidget * parent=0,
-			  const char * name=0,
-			  Qt::WFlags f=0);
+  ofstream outs(argv[2]);
 
-    virtual ~gvApplicationVolAndLR();
+  shared_ptr<Cylinder> under_surf = dynamic_pointer_cast<Cylinder>(in_surf->underlyingSurface());
+  RectDomain rd = under_surf->parameterDomain();
 
-public slots:
-    virtual void view_reset();
+  for (int i = 0; i < 2; ++i)
+    {
+      double from_upar = (i == 0) ? rd.umin() : M_PI;
+      double to_upar = (i == 0) ? M_PI : rd.umax();
+      double from_vpar = rd.vmin();
+      double to_vpar = rd.vmax();
+      cout << "Splitting to [" << from_upar << ", " << to_upar << "]x[" << from_vpar << ", " << to_vpar << "]" << endl;
+      vector<shared_ptr<ParamSurface> > sub_surfs = in_surf->subSurfaces(from_upar, from_vpar, to_upar, to_vpar);
+      cout << "Splitting completed, number of surfaces is " << sub_surfs.size() << endl;
+      sub_surfs[0]->writeStandardHeader(outs);
+      sub_surfs[0]->write(outs);
+    }
+  outs.close();
 
-    void translate_to_origin(); // All selected objects are translated by the center of their bounding box.
-    void move_vertices_to_origin(); // All selected objects are translated by the center of their bounding box.
-
-protected:
-    void buildExtraGUI();
-
-private:
-    Go::GeneralMesh* getMesh(int object_id);
-
-};
-
-#endif // _GVAPPLICATIONVOLANDLR_H
-
+}
