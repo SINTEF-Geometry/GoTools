00570d61c4f337f4639c3ac3c43bb3b9b398dbcd
Sverre Briseid <Sverre.Briseid@sintef.no>
2015-03-18 13:02:39 +0100

    * Added OpenMP support for LRSplineMBA::MBAUpdate(). Not activated as method is slightly slower than without OpenMP, needs rewrite to avoid omp critical pragmas.

--- lrsplines2D/src/LRSplineMBA.C
+++ lrsplines2D/src/LRSplineMBA.C
@@ -299,8 +299,6 @@ void LRSplineMBA::MBAUpdate(LRSplineSurface *srf)
       //vector<double>& ghost_points = el1->second->getGhostPoints();
       vector<double> ghost_points;
 
-       tmp_weights.resize(bsplines.size());
-      
       // Compute contribution from all points
       int ki, kk;
       size_t kj;
@@ -316,45 +314,14 @@ void LRSplineMBA::MBAUpdate(LRSplineSurface *srf)
       int threadId = 0;
 
 //      printf("tmp_weights.size(): %i\n", tmp_weights.size());
-      // @@sbr Not thread safe!
-#if 1//ndef _OPENMP
-//      cout << "LRSplineMBA: OpenMP turned off!" << endl;
       {
       for (ki=0, curr=&points[0]; ki<nmb_pts; ++ki, curr+=del)
       {
-#else
-	  assert(dim == 1);
-	  // omp_set_num_threads(1);
-	  // pthread_attr_t attr;
-	  // size_t stacksize;
-	  // int status = pthread_attr_getstacksize(&attr, &stacksize);
-	  // std::cout << "status: " << status << std::endl;
-	  // status = pthread_attr_setstacksize(&attr, 3);//&attr, &stacksize);
-	  // std::cout << "status: " << status << std::endl;
-	  // status = pthread_attr_getstacksize(&attr, &stacksize);
-	  // std::cout << "status: " << status << std::endl;
-	  // std::cout << "stacksize (in MB): " << (double)stacksize/(1024.0*1024.0) << std::endl;
-#pragma omp parallel default(none) private(ki, kj, kk, u_at_end, v_at_end, total_squared_inv, val, wgt, wc, phi_c, curr, nom_denom, gamma, threadId) firstprivate(tmp, tmp_weights) shared(nmb_pts, points, umax, vmax, tol, dim, del, bsplines)
-	  {
-	      // printf("bsplines_size: %i\n", bsplines_size);
-	      // threadId = omp_get_thread_num();
-	      // snprintf("ki: ", ki, "omp-%02d", threadId);
-//	      printf("threadId: %i\n", threadId);
-//	    vector<double> tmp_weights(bsplines_size);
-
-#pragma omp for schedule(runtime) //dynamic, 4)//static, 4)//runtime)//guided)//auto)
-	      for (ki=0; ki<nmb_pts; ++ki)
-	      {
-		  curr = &points[ki*del];
-		  threadId = omp_get_thread_num();
-#endif
-//		  snprintf("ki: ", ki, "omp-%02d", threadId);
 		  // printf("ki: %i\n", ki);
 		  // printf("del: %i\n", del);
 		  // printf("points.size(): %i\n", points.size());
 		   // printf("curr[0]: %d\n", curr[0]);
 		  // printf("curr[1]: %f\n", curr[1]);
-//#endif//_OPENMP
 		  // Computing weights for this data point
 		  u_at_end = (curr[0] > umax-tol) ? true : false;
 		  v_at_end = (curr[1] > vmax-tol) ? true : false;
@@ -386,20 +353,13 @@ void LRSplineMBA::MBAUpdate(LRSplineSurface *srf)
 		      {
 			  phi_c = wc * curr[del-dim+kk] * total_squared_inv;
 			  tmp[kk] = wc * wc * phi_c;
-		      } // @@sbr201412 But is this threadsafe? Do not think so.
- // This construction makes all assignments in the for loop run in serial ...
-#if 0
-#pragma omp critical
-#endif
-// #pragma omp barrier // Takes forever ...
-//#pragma omp atomic // Single operation (not function) only.
-			  add_contribution(dim, nom_denom, bsplines[kj], &tmp[0], 
-			  		   wc * wc);
+		      }
+		      add_contribution(dim, nom_denom, bsplines[kj], &tmp[0], 
+				       wc * wc);
 		  }
 		  // printf("Done with for loop.\n");
 	      }
 	  }
-	  // printf("Done with OpenMP.\n");
 
 	  // Compute contribution from ghost points
 	  for (ki=0, curr=&ghost_points[0]; ki<nmb_ghost; ++ki, curr+=del)
@@ -450,6 +410,221 @@ void LRSplineMBA::MBAUpdate(LRSplineSurface *srf)
   double fac = 1.0; //1.01;
   srf->addSurface(*cpsrf, fac);
 
+ }
+
+
+//==============================================================================
+void LRSplineMBA::MBAUpdate_omp(LRSplineSurface *srf)
+//==============================================================================
+{
+  double tol = 1.0e-12;  // Numeric tolerance
+
+  double umax = srf->endparam_u();
+  double vmax = srf->endparam_v();
+
+  // Make a copy of the surface
+  shared_ptr<LRSplineSurface> cpsrf(new LRSplineSurface(*srf));
+
+  // Set all coefficients to zero (keep the scaling factors)
+  int dim = srf->dimension();
+  Point coef(dim);
+  coef.setValue(0.0);
+  for (LRSplineSurface::BSplineMap::const_iterator it1 = cpsrf->basisFunctionsBegin();
+       it1 != cpsrf->basisFunctionsEnd(); ++it1)
+    cpsrf->setCoef(coef, it1->second.get());
+    
+  // Map to accumulate numerator and denominator to compute final coefficient value
+  // for each BSplineFunction
+  map<const LRBSpline2D*, Array<double,2> > nom_denom; 
+
+  vector<LRSplineSurface::ElementMap::const_iterator> el1_vec;
+  const int num_elem = srf->numElements();
+  el1_vec.reserve(num_elem);
+  for (LRSplineSurface::ElementMap::const_iterator iter = srf->elementsBegin(); iter != srf->elementsEnd(); ++iter)
+  {
+      el1_vec.push_back(iter);
+  }
+  vector<LRSplineSurface::ElementMap::const_iterator> el2_vec;
+  el2_vec.reserve(cpsrf->numElements());
+  for (LRSplineSurface::ElementMap::const_iterator iter = cpsrf->elementsBegin(); iter != cpsrf->elementsEnd(); ++iter)
+  {
+      el2_vec.push_back(iter);
+  }
+
+  // Traverse all elements. The two surfaces will have corresponding elements,
+  // but only the source surface elements will contain point information so 
+  // the elements in both surfaces must be traversed
+  int del = 3 + dim;  // Parameter pair, position and distance between surface and point
+  LRSplineSurface::ElementMap::const_iterator el1;
+  LRSplineSurface::ElementMap::const_iterator el2;
+  int kl;
+#ifdef _OPENMP
+  // const int num_threads = 1;
+  // omp_set_num_threads(num_threads);
+#pragma omp parallel default(none) private(kl, el1, el2) shared(nom_denom, tol, dim, el1_vec, el2_vec, umax, vmax, del)
+#pragma omp for schedule(auto)//guided)//static,8)//runtime)//dynamic,4)
+#else
+  MESSAGE("OpenMP support is missing, method should not be called!");
+  return;
+#endif
+  for (kl = 0; kl < num_elem; ++kl)
+  {
+      el1 = el1_vec[kl];
+      if (!el1->second->hasDataPoints())
+	continue;  // No points to use in surface update
+      el2 = el2_vec[kl];
+
+// #ifndef NDEBUG
+// #ifdef _OPENMP
+//       int num_threads = omp_get_num_threads();
+//       printf("kl: %i, num_threads: %i\n", kl, num_threads);
+// #endif
+// #endif
+
+      vector<double> tmp(dim);
+      // Temporary vector to store weights associated with a given data point
+      vector<double> tmp_weights;  
+
+      // Fetch associated B-splines belonging to the difference surface
+      const vector<LRBSpline2D*>& bsplines = el2->second->getSupport();
+
+      const int bsplines_size = bsplines.size();
+
+     // Check if the element needs to be updated
+      size_t nb;
+      for (nb=0; nb<bsplines.size(); ++nb)
+	if (!bsplines[nb]->coefFixed())
+	  break;
+
+      if (nb == bsplines.size())
+	continue;   // Element satisfies accuracy requirements
+
+      // Fetch points from the source surface
+      int nmb_pts = el1->second->nmbDataPoints();
+      vector<double>& points = el1->second->getDataPoints();
+      int nmb_ghost = 0; //el1->second->nmbGhostPoints();
+      //vector<double>& ghost_points = el1->second->getGhostPoints();
+      vector<double> ghost_points;
+
+      // Compute contribution from all points
+      int ki, kk;
+      size_t kj;
+      const double *curr;
+      bool u_at_end, v_at_end;
+      double total_squared_inv, val, wgt, wc, phi_c, gamma;
+      tmp_weights.resize(bsplines.size());
+      // std::cout << "tmp_weight.size(): " << tmp_weights.size() << std::endl;
+      // std::cout << "nmb_pts: " << nmb_pts << std::endl;
+      // std::cout << "points.size(): " << points.size() << std::endl;
+      // std::cout << "dim: " << dim << std::endl;
+      // std::cout << "del: " << del << std::endl;
+      int threadId = 0;
+
+//      printf("tmp_weights.size(): %i\n", tmp_weights.size());
+      // @@sbr Not thread safe!
+      {
+      for (ki=0, curr=&points[0]; ki<nmb_pts; ++ki, curr+=del)
+      {
+		  // printf("ki: %i\n", ki);
+		  // printf("del: %i\n", del);
+		  // printf("points.size(): %i\n", points.size());
+		   // printf("curr[0]: %d\n", curr[0]);
+		  // printf("curr[1]: %f\n", curr[1]);
+		  // Computing weights for this data point
+		  u_at_end = (curr[0] > umax-tol) ? true : false;
+		  v_at_end = (curr[1] > vmax-tol) ? true : false;
+		  total_squared_inv = 0.0;
+		  for (kj=0; kj<bsplines.size(); ++kj) 
+		  {
+		      // printf("umin: %f\n", bsplines[kj]->umin());
+		      // printf("vmin: %f\n", bsplines[kj]->vmin());
+		      val = bsplines[kj]->evalBasisFunction(curr[0], curr[1], 0, 0,
+							    u_at_end, v_at_end);
+		      gamma = bsplines[kj]->gamma();
+		      wgt = val*gamma;//bsplines[kj]->gamma();
+		      // printf("kj: %i\n", kj);
+		      // printf("tmp_weights.size(): %i\n", tmp_weights.size());
+		      tmp_weights[kj] = wgt;
+		      total_squared_inv += wgt*wgt;
+		      // printf("total_squared_inv: %f\n", total_squared_inv);
+		  }
+		  // printf("Done with for loop.\n");
+		  total_squared_inv = (total_squared_inv < tol) ? 0.0 : 1.0/total_squared_inv;
+		  // printf("total_squared_inv: %f\n", total_squared_inv);
+
+		  // Compute contribution
+		  for (kj=0; kj<bsplines.size(); ++kj)
+		  {
+		      // printf("kj: %i\n", kj);
+		      wc = tmp_weights[kj]; 
+		      for (kk=0; kk<dim; ++kk)
+		      {
+			  phi_c = wc * curr[del-dim+kk] * total_squared_inv;
+			  tmp[kk] = wc * wc * phi_c;
+		      }
+#if 1
+#pragma omp critical // Needed to make for loop thread safe. Slows down performance, should be circumvented.
+#endif
+		       add_contribution(dim, nom_denom, bsplines[kj], &tmp[0], 
+					wc * wc);
+		  }
+		  // printf("Done with for loop.\n");
+	      }
+	  }
+	  // printf("Done with OpenMP.\n");
+
+	  // Compute contribution from ghost points
+	  for (ki=0, curr=&ghost_points[0]; ki<nmb_ghost; ++ki, curr+=del)
+	  {
+	      // Computing weights for this data point
+	      bool u_at_end = (curr[0] > umax-tol) ? true : false;
+	      bool v_at_end = (curr[1] > vmax-tol) ? true : false;
+	      double total_squared_inv = 0;
+	      for (kj=0; kj<bsplines.size(); ++kj) 
+	      {
+		  double val = bsplines[kj]->evalBasisFunction(curr[0], curr[1], 0, 0,
+							       u_at_end, v_at_end);
+		  const double wgt = val*bsplines[kj]->gamma();
+		  tmp_weights[kj] = wgt;
+		  total_squared_inv += wgt*wgt;
+	      }
+	      total_squared_inv = (total_squared_inv < tol) ? 0.0 : 1.0/total_squared_inv;
+
+	      // Compute contribution
+	      for (kj=0; kj<bsplines.size(); ++kj)
+	      {
+		  const double wc = tmp_weights[kj]; 
+	      for (int ka=0; ka<dim; ++ka)
+		  {
+		  const double phi_c = wc * curr[del-dim+ka] * total_squared_inv;
+		  tmp[ka] = wc * wc * phi_c;
+		  }
+#if 1
+#pragma omp critical // Needed to make for loop thread safe. Slows down performance, should be circumvented.
+#endif
+		  add_contribution(dim, nom_denom, bsplines[kj], &tmp[0], 
+				   wc * wc);
+	      }
+	  }
+  }
+
+  // Compute coefficients of difference surface
+  LRSplineSurface::BSplineMap::const_iterator it1 = cpsrf->basisFunctionsBegin();
+  LRSplineSurface::BSplineMap::const_iterator it2 = srf->basisFunctionsBegin();
+  for (; it1 != cpsrf->basisFunctionsEnd(); ++it1, ++it2) 
+    {
+      auto nd_it = nom_denom.find(it2->second.get());
+      const auto& entry = nd_it->second;
+      Point coef(dim);
+      for (int ka=0; ka<dim; ++ka)
+	coef[ka] = (fabs(entry[dim]<tol)) ? 0 : entry[ka] / entry[dim];
+      cpsrf->setCoef(coef, it1->second.get());
+    }
+ 
+  // Update initial surface
+  double fac = 1.0; //1.01;
+  srf->addSurface(*cpsrf, fac);
+
 // #ifdef _OPENMP
 //   double time1 = omp_get_wtime();
 //   double time_spent = time1 - time0;
@@ -457,6 +632,7 @@ void LRSplineMBA::MBAUpdate(LRSplineSurface *srf)
 //   // std::cout << "time_spent in for loop: " << time_loop << std::endl;
 // #endif
  }
+
 
 //==============================================================================
   void LRSplineMBA::MBAUpdate(LRSplineSurface *srf,
