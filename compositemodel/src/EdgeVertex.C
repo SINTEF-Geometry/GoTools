e68718b40ff6165986bb1ff172d64cd24d32bc03
Sverre Briseid <sverre.briseid@sintef.no>
2017-10-03 11:22:52 +0200

    EdgeVertex::reOrganize(): Bugfix, removed infinite loop.

--- compositemodel/src/EdgeVertex.C
+++ compositemodel/src/EdgeVertex.C
@@ -426,7 +426,7 @@ namespace Go
     // The method requires all edges to have their face assigned. This may not be the case when building
     // the topology or while reading from file.
     size_t ki;
-    for (ki=0; ki<edges.size();)
+    for (ki=0; ki<edges.size(); ++ki)
     {
         ftFaceBase* face_base = edges[ki]->face();
 	ftSurface *curr_face = (face_base != NULL) ? face_base->asFtSurface() : NULL;
@@ -439,6 +439,7 @@ namespace Go
     size_t kj, kr;
     // Reorganization is limited within one Body or for edges that
     // do not belong to a body
+     // @@sbr201710 Why not use 'while (edges.size() > 0)', easier to understand the test criteria.
     for (ki=0; ki<edges.size();)
     {
         ftFaceBase* face_base = edges[ki]->face();
@@ -614,7 +615,7 @@ namespace Go
 	    curr_edges.erase(curr_edges.begin()+std::min(kj, kmin));
 	    k2--;
 	  }
-      }
+    }
 
     // Make sure that edges with twins are placed first
     for (ki=0; ki<edges_.size(); ++ki)
