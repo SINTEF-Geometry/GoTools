2cf342c3f2916af6f44391ba38c09c036a736227
Sverre Briseid <sverre.briseid@sintef.no>
2018-02-02 16:02:41 +0100

    Added missing file.

---
+++ viewlib/src/PointSizeSheet.C
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 1998, 2000-2007, 2010, 2011, 2012, 2013 SINTEF ICT,
+ * Applied Mathematics, Norway.
+ *
+ * Contact information: E-mail: tor.dokken@sintef.no                      
+ * SINTEF ICT, Department of Applied Mathematics,                         
+ * P.O. Box 124 Blindern,                                                 
+ * 0314 Oslo, Norway.                                                     
+ *
+ * This file is part of GoTools.
+ *
+ * GoTools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version. 
+ *
+ * GoTools is distributed in the hope that it will be useful,        
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of         
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with GoTools. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public
+ * License, a covered work must retain the producer line in every data
+ * file that is created or manipulated using GoTools.
+ *
+ * Other Usage
+ * You can be released from the requirements of the license by purchasing
+ * a commercial license. Buying such a license is mandatory as soon as you
+ * develop commercial activities involving the GoTools library without
+ * disclosing the source code of your own applications.
+ *
+ * This file may be used in accordance with the terms contained in a
+ * written agreement between you and SINTEF ICT. 
+ */
+
+
+#include "GoTools/viewlib/PointSizeSheet.h"
+#include "GoTools/viewlib/ui_PointSizeSheet_form.h"
+
+#include <QPushButton>
+#include <QSlider>
+#include <QCheckBox>
+#include <iostream>
+
+using namespace Ui;
+
+namespace {
+    const double MAX_POINT_SIZE = 10.0;
+}
+
+//===========================================================================
+void PointSizeSheet::createSheet(QWidget* parent, gvObserver* obs)
+//===========================================================================
+{
+    obs_ = obs;
+    form_ = new Ui::PointSizeSheet_form();//parent);
+
+    QDialog* dial = new QDialog();
+    form_->setupUi(dial);
+
+    dial->resize(form_->box->size());
+
+    QObject::connect(form_->OkButton, SIGNAL(clicked()),
+		     this, SLOT(ok()));
+    QObject::connect(form_->CancelButton, SIGNAL(clicked()),
+		     dial, SLOT(close()));
+    int pspercent = int(size_/MAX_POINT_SIZE*100.0 + 0.5);
+    form_->SizeSlider->setValue(pspercent);
+    form_->LCDNumber1->display(pspercent);
+//    form_->LCDNumber1->intValue->setValue(size_);
+
+    dial->show();
+}
+
+
+//===========================================================================
+void PointSizeSheet::ok()
+//===========================================================================
+{
+    //size_ = form_->SizeSlider->value();
+
+    size_ = double(form_->SizeSlider->value()) * MAX_POINT_SIZE / 100.0;
+
+    emit return_value(size_);
+//     form_->close();
+}
+
