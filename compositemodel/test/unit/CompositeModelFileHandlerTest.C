f62a50c2c34ea660db32c6a6d3d628c3eac63799
Sverre Briseid <Sverre.Briseid@sintef.no>
2018-10-11 15:26:57 +0200

    Unit test to trigger bug with edge topology around vertices.

---
+++ compositemodel/test/unit/CompositeModelFileHandlerTest.C
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 1998, 2000-2007, 2010, 2011, 2012, 2013 SINTEF ICT,
+ * Applied Mathematics, Norway.
+ *
+ * Contact information: E-mail: tor.dokken@sintef.no                      
+ * SINTEF ICT, Department of Applied Mathematics,                         
+ * P.O. Box 124 Blindern,                                                 
+ * 0314 Oslo, Norway.                                                     
+ *
+ * This file is part of GoTools.
+ *
+ * GoTools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version. 
+ *
+ * GoTools is distributed in the hope that it will be useful,        
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of         
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with GoTools. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public
+ * License, a covered work must retain the producer line in every data
+ * file that is created or manipulated using GoTools.
+ *
+ * Other Usage
+ * You can be released from the requirements of the license by purchasing
+ * a commercial license. Buying such a license is mandatory as soon as you
+ * develop commercial activities involving the GoTools library without
+ * disclosing the source code of your own applications.
+ *
+ * This file may be used in accordance with the terms contained in a
+ * written agreement between you and SINTEF ICT. 
+ */
+
+
+#define BOOST_TEST_MODULE CompositeModelFileHandlerTest
+#include <boost/test/included/unit_test.hpp>
+
+#include <fstream>
+#include "GoTools/compositemodel/CompositeModelFileHandler.h"
+
+using namespace Go;
+using std::vector;
+using std::ifstream;
+using std::string;
+
+struct Config {
+public:
+    Config()
+    {
+
+#ifdef GOTOOLS_TEST_PRIVATE_DATA
+        const std::string data_basedir("../../gotools-private-data/step_reader/data2");
+        filenames.push_back(data_basedir+"/s30.stp.g22");
+#endif
+
+    }
+
+public:
+    vector<string> filenames;
+    vector<double> offset, epsgeo;
+};
+
+
+BOOST_FIXTURE_TEST_CASE(CompositeModelFileHandlerTest, Config)
+{
+
+    for (size_t ki = 0; ki < filenames.size(); ++ki)
+    {
+
+        ifstream infile(filenames[ki]);
+        if (!(infile.good()))
+        {
+            BOOST_ERROR("Input file not found or file corrupted!");
+            continue;
+        }
+
+        Go::CompositeModelFileHandler go_file_handler;
+        vector<shared_ptr<Go::SurfaceModel> > surf_models = go_file_handler.readSurfModels(filenames[ki].c_str());
+
+        if (surf_models.size() != 1)
+        {
+            BOOST_ERROR("Failed reading the SurfaceModel.");
+            continue;
+        }
+
+        bool top_ok = surf_models[0]->checkShellTopology();
+
+        // Since we may need to handle self intersections and kinks in the input surface set we are not
+        // guaranteed that we are within the offset-dist +/- epsgeo for all points. Assuming that the
+        // method is capable of reporting on the status.
+        BOOST_CHECK_EQUAL(top_ok, true);
+    }
+}
