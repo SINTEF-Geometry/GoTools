cf21a28154559eec96a530bdd2c0fbabc474ca19
Sverre Briseid <Sverre.Briseid@sintef.no>
2024-08-26 16:05:41 +0200

    Empty init() function to be used when logging is disabled.

--- gotools-core/include/GoTools/utils/Logger.h
+++ gotools-core/include/GoTools/utils/Logger.h
@@ -97,6 +97,9 @@ inline void init() {
 #define LOG_CRITICAL BOOST_LOG_TRIVIAL(fatal)
 
 #else
+
+inline void init() {} // Empty init function when logging is disabled
+
 #define LOG_TRACE(...)   std::cout << "[TRACE] " << __VA_ARGS__ << std::endl
 #define LOG_DEBUG(...)   std::cout << "[DEBUG] " << __VA_ARGS__ << std::endl
 #define LOG_INFO(...)    std::cout << "[INFO] " << __VA_ARGS__ << std::endl
@@ -105,5 +108,4 @@ inline void init() {
 #define LOG_CRITICAL(...) std::cerr << "[CRITICAL] " << __VA_ARGS__ << std::endl
 #endif
 
-void init();
-}
+} // namespace Go::Logger
